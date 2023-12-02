81d9395282d3e4904946edfcfe529f316e3dfc4d
XYJ080 <101054474+JY251@users.noreply.github.com>
2023-12-02 21:56:18 +0900

    AC: 個数ではなく金額を出力するDP

--- 1202/b_4.cpp
+++ 1202/b_4.cpp
@@ -19,18 +19,18 @@ int main() {
 	for (int i=0; i<=N; i++) {
 		for (int j=0; j<=2; j++) {
 			if (j==0) {
-				twoDimVector[i][j] = (i / 6) + (i % 6 != 0);
+				twoDimVector[i][j] = ((i / 6) + (i % 6 != 0))*S;
 			} else if (j==1) {
 				// for loopで、
 				for (int k=0; k<i; k++) {
-					twoDimVector[i][j] = min(twoDimVector[i][j], twoDimVector[k][j-1] + (i-k)/8 + ((i-k)%8 != 0));
+					twoDimVector[i][j] = min(twoDimVector[i][j], twoDimVector[k][j-1] + ((i-k)/8 + ((i-k)%8 != 0))*M);
 				}
 				twoDimVector[i][j] = min(twoDimVector[i][j], twoDimVector[i][j-1]);
 			} else {
 				for (int k=0; k<i; k++) {
 					// cout << "start" << endl;
 					// cout << twoDimVector[k][j-1] + (i-k)/12 + ((i-k)%12 != 0) << endl;
-					twoDimVector[i][j] = min(twoDimVector[i][j], twoDimVector[k][j-1] + (i-k)/12 + ((i-k)%12 != 0));
+					twoDimVector[i][j] = min(twoDimVector[i][j], twoDimVector[k][j-1] + ((i-k)/12 + ((i-k)%12 != 0))*L);
 				}
 				twoDimVector[i][j] = min(twoDimVector[i][j], twoDimVector[i][j-1]);
 			}
