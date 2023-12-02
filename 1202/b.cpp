7fe0f8a186c77df23abf09ffdcd61328ac4e6880
XYJ080 <101054474+JY251@users.noreply.github.com>
2023-12-02 21:16:49 +0900

    S only, M only, L onlyの３つだけを考える

---
+++ 1202/b.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int N, S, M, L;
+	cin >> N >> S >> M >> L;
+
+	int minCostS = (N / 6 + (N % 6 != 0)) * S;
+	int minCostM = (N / 8 + (N % 8 != 0)) * M;
+	int minCostL = (N / 12 + (N % 12 != 0)) * L;
+
+	int result = min({minCostS, minCostM, minCostL});
+
+	cout << result << endl;
+	
+
+	return 0;
+}
