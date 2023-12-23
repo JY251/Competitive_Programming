7fa43f3fac082c3842c1aced821c21c83c194751
XYJ080 <101054474+JY251@users.noreply.github.com>
2023-12-23 21:25:56 +0900

    OK

---
+++ ABC334/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int B;
+	int G;
+
+	cin >> B >> G;
+
+	if (B > G) {
+		cout << "Bat" << endl;
+	} else {
+		cout << "Glove" << endl;
+	}
+	return 0;
+}
