8c3d14344dcddfa7c4f4ba1e37fb233563fa5879
JY251 <101054474+JY251@users.noreply.github.com>
2024-01-06 23:55:13 +0900

    revised

--- ABC334/int_size.cpp
+++ ABC334/int_size.cpp
@@ -3,7 +3,7 @@
 // 4 bytes
 int main() {
     std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
-    std::cout << "Size of int: " << sizeof(long) << " bytes" << std::endl;
-    std::cout << "Size of int: " << sizeof(long long) << " bytes" << std::endl;
+    std::cout << "Size of long: " << sizeof(long) << " bytes" << std::endl;
+    std::cout << "Size of long long: " << sizeof(long long) << " bytes" << std::endl;
     return 0;
 }
