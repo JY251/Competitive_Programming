a912e5ffa8885134b99a071b20f58b662a542393
JY251 <101054474+JY251@users.noreply.github.com>
2024-01-14 21:49:30 +0900

    sub1

---
+++ ABC336/e.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+long calculateDigitSum(long n) {
+	long sum = 0;
+
+	while(n > 0) {
+		sum += n % 10;
+		n /= 10;
+	}
+	return sum;
+}
+
+long countGoodNumbers(long n) {
+	long count = 0;
+
+	for (long i = 1; i <= N; i++) {
+		if (i % calculateDigitSum(i) == 0) {
+			count++;
+		}
+	}
+
+	return count;
+}
+
+int main() {
+	long N;
+	cin >> N;
+
+	cout << countGoodNumbers(N) << endl;
+	return 0;
+}
