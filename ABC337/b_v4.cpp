2acb7ab311e7fc298f4147e29f8c9b459cf146d1
JY251 <101054474+JY251@users.noreply.github.com>
2024-01-20 21:34:45 +0900

    sub2

---
+++ ABC337/b_v4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isExtendedABC(const string& s) {
+	if (s.empty()) {
+		return true;
+	}
+
+	const char* ptr = s.c_str();
+
+	while (*ptr == 'A') {
+		ptr++;
+	}
+
+	if (*ptr != 'B' && *ptr != 'C' && *ptr != '\0') {
+		return false;
+	}
+
+	while ((*ptr == 'B')) {
+		ptr++;
+	}
+
+	if (*ptr != 'C' && *ptr != '\0') {
+		return false;
+	}
+
+	while (*ptr == 'C') {
+		ptr++;
+	}
+
+	if (*ptr != '\0') {
+		return false;
+	}
+
+	return true;
+}
+
+int main() {
+	string s;
+	cin >> s;
+
+	bool result = isExtendedABC(s);
+
+	if (result) {
+		cout << "Yes" << endl;
+	}	else {
+		cout << "No"  << endl;
+	}
+
+	return 0;
+}
