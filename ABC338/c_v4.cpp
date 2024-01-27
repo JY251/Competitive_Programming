1920a1d29a2a5e37e1d02415e41f515ed3fe8520
JY251 <101054474+JY251@users.noreply.github.com>
2024-01-27 22:35:40 +0900

    sub2: case B with 37

--- ABC338/c_v4.cpp
+++ ABC338/c_v4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -10,27 +11,45 @@ int max_servings(vector<int>& Q, vector<int>& A, vector<int>& B) {
 	vector<int> max_per_ingredient(N, 0);
 
 	for (int i = 0; i < N; i++) {
-		max_per_ingredient[i] = Q[i] / A[i];
+		if (A[i] != 0) {
+			max_per_ingredient[i] = Q[i] / A[i];
+		} else {
+			max_per_ingredient[i] = INT_MAX;
+		}
 	}
 
 	int max_servings = *min_element(max_per_ingredient.begin(), max_per_ingredient.end());
 
+	// cout << "max_servings (line 24): " << max_servings << endl; // OK
+
 	vector<int> dp(max_servings, 0);
 
 	dp[0] = max_servings;
-	for (int i = 0; i < max_servings; i++) {
+	for (int i = 1; i < max_servings; i++) {
 		int max_servings_A = dp[0] - i;
 
 		vector<int> max_per_ingredient_B(N, 0);
 		for (int j = 0; j < N; j++) {
-			max_per_ingredient_B[j] = (Q[j] - max_servings_A * A[j]) / B[j];
+			if (B[j] != 0) {
+				max_per_ingredient_B[j] = (Q[j] - max_servings_A * A[j]) / B[j];
+			} else {
+				max_per_ingredient_B[j] = INT_MAX;
+			}
+			// max_per_ingredient_B[j] = (Q[j] - max_servings_A * A[j]) / B[j];
 		}
 		int max_servings_B = *min_element(max_per_ingredient_B.begin(), max_per_ingredient_B.end());
+
+		// cout << "max_servings_A (line 42): " << max_servings_A << " j: " << i << endl; // OK
 
 		dp[i] = max_servings_A + max_servings_B;
 	}
 
 	max_servings = *max_element(dp.begin(), dp.end());
+
+	// for (int i = 0; i < max_servings; i++) {
+	// 	cout << dp[i] << " ";
+	// }
+	// cout << endl;
 	return max_servings;
 }
 
