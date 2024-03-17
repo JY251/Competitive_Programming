0729eddc0042f5ad87bcc52386719867a588d6b6
JY251 <101054474+JY251@users.noreply.github.com>
2024-03-17 22:31:47 +0900

    Revert "use __int128 but failed"
    This reverts commit b886d1124098239681a46db8c054251db2eb4623.

--- ARC174/1.cpp
+++ ARC174/1.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <vector>
-#include <boost/multiprecision/cpp_int.hpp>
 
-using boost::multiprecision::int128_t;
+using namespace std;
 
-vector<__int128> cumulative_sum(const vector<int> &A) {
-	vector<__int128> S(A.size() + 1, 0);
+vector<long long> cumulative_sum(const vector<int> &A) {
+	vector<long long> S(A.size() + 1, 0);
 	S[0] = 0;
 	for (int i = 1; i <= A.size(); i++) {
 		S[i] = S[i - 1] + A[i-1];
@@ -21,14 +20,14 @@ int main() {
 		cin >> A[i];
 	}
 
-	vector <__int128> S = cumulative_sum(A);
+	vector <long long> S = cumulative_sum(A);
 
-	vector <__int128> dp(N + 1, 0);
+	vector <long long> dp(N + 1, 0);
 	for (int i = 1; i <= N; i++) {
 		for (int l = 1; l <= i; l++) {
 			for (int r = 1; r <= i; r++) {
-				__int128 sum_with_multiplication = (S[r] - S[l - 1]) * C + S[i] - S[r];
-				__int128 sum_without_multiplication = S[i] - S[l - 1];
+				long long sum_with_multiplication = (S[r] - S[l - 1]) * C + S[i] - S[r];
+				long long sum_without_multiplication = S[i] - S[l - 1];
 
 				dp[i] = max(dp[i], S[l - 1] + max(sum_with_multiplication, sum_without_multiplication));
 			}
