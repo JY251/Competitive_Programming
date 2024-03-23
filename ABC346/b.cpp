f90c5427cafcc84b1109762ef69d4a3696c44a05
JY251 <101054474+JY251@users.noreply.github.com>
2024-03-23 21:27:46 +0900

    2nd Sub (work for all example 1 -3)

--- ABC346/b.cpp
+++ ABC346/b.cpp
@@ -13,13 +13,14 @@ int main() {
 	for (int len = 1; len <= n; len++) {
 		for (int i = 0; i + len <= n; i++) {
 			int w_count = 0, b_count = 0;
-			for (int j = i; j < i + len; j++) {
+			for (int j = i; j < i + W + B; j++) {
 				if (S[(j%n)] == 'w') {
 					w_count++;
 				} else {
 					b_count++;
 				}
 			}
+			// cout << "w_count: " << w_count << " b_count: " << b_count << endl; // "w_count: 0 b_count: 12
 			if (w_count == W && b_count == B) {
 				cout << "Yes" << endl;
 				return 0;
