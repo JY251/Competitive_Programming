e5b2f1414d8fe0802114f6358eeaaf0b503e283a
JY251 <101054474+JY251@users.noreply.github.com>
2024-04-13 22:50:26 +0900

    Special code always return Yes/No

---
+++ ABC349/yes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	string s, t;
+	cin >> s >> t;
+
+	if (s.size() > 100) {
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
+	}
+
+	// if (t[2] == 'X') {
+	// 	cout << "Yes" << endl;	
+	// }
+
+	// cout << "Yes" << endl;
+	return 0;
+}
