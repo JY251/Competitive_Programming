9403ed3bda25ce870aa58d5d080d989cb60352e2
JY251 <101054474+JY251@users.noreply.github.com>
2024-04-13 21:41:18 +0900

    Sub2: add notes

--- ABC349/b.cpp
+++ ABC349/b.cpp
@@ -10,21 +10,15 @@ bool isGoodString(string s) {
 		charCount[c]++;
 	}
 
-	for (auto it: charCount) {
-		cout << it.first << " " << it.second << endl;
-	}
-
 	vector<int> i_counts(s.size(), 0);
 
 	for (auto it: charCount) {
 		i_counts[it.second - 1]++; 
-		cout << it.second << endl;
+		// if here is [it.second], then string such as "aaa" will be considered as good string
+		// because it.second = 3, and i_counts[3] will be 1, but in the next for loop, loop moves from 0 to 2, so i_counts[3] will not be checked
 	}
 
-	for (int i = 0; i < i_counts.size(); i++) {
-		int i_count = i_counts[i];
-	// for (int i_count: i_counts) {
-		cout << i_count << endl;
+	for (int i_count: i_counts) {
 		if (i_count != 2 && i_count != 0) {
 			return false;
 		}
