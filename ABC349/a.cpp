7b79e3c200cc64a8fc5e65ab316d24edea7d7a8e
JY251 <101054474+JY251@users.noreply.github.com>
2024-04-13 21:16:10 +0900

    sub1

--- ABC349/a.cpp
+++ ABC349/a.cpp
@@ -11,17 +11,13 @@ int main() {
 		cin >> scores[i];
 	}
 
-	for (int i = 0; i < N; ++i) {
-		scores[i] += N - i;
+	int final_score_N = 0;
+
+	for (int i = 0; i < N - 1; ++i) {
+		final_score_N += scores[i];
 	}
 
-	int final_score_N = max(0, min(scores[0], 100));
-
-	for (int i = 1; i < N - 1; ++i) {
-		final_score_N = max(final_score_N, min(scores[i], 100));
-	}
-
-	cout << final_score_N << endl;
+	cout << (0 - final_score_N) << endl;
 
 	return 0;
 }
