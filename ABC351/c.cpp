c1ba6b19c7ba71c643c32c4d21dd5e484503835d
JY251 <101054474+JY251@users.noreply.github.com>
2024-04-27 22:05:06 +0900

    revised & debug

--- ABC351/c.cpp
+++ ABC351/c.cpp
@@ -9,13 +9,14 @@ int main() {
 
 	vector<int> A(N, 0);
 	for (int i = 0; i < N; i++) {
-		A[i] = 0;
+		cin >> A[i];
 	}
 
 	vector<int> column;
 	for (int i = 1; i <= N; i++) {
 		// i is 1-indexed; A is 0-indexed
 		column.push_back(A[i-1]);
+		cout << "column num (after push i-th element): " << column.size() << endl;
 		if (!(column.size() <= 1)) {
 			if (column[column.size()-1] == column[column.size()-2]) {
 				int a = column[column.size()-1];
@@ -25,6 +26,7 @@ int main() {
 				column.push_back(a+b);
 			}
 		}
+		cout << "column num (after pop if necessary): " << column.size() << endl;
 	}
 
 	cout << column.size() << endl;
