851759a96f4b321553c318c48978a1b52878728e
JY251 <101054474+JY251@users.noreply.github.com>
2024-04-27 22:23:28 +0900

    Sub: AC

--- ABC351/c.cpp
+++ ABC351/c.cpp
@@ -16,11 +16,6 @@ int main() {
 	for (int i = 1; i <= N; i++) {
 		// i is 1-indexed; A is 0-indexed
 		column.push_back(A[i-1]);
-		cout << "column num (after push " << i << "-th element): " << column.size() << endl;
-		for (int j = 0; j < column.size(); j++) {
-			cout << column[j] << " ";
-		}
-		cout << endl;
 		while (!(column.size() <= 1) && (column[column.size()-1] == column[column.size()-2])) {
 				int a = column[column.size()-1];
 				// int b = column[column.size()-2];
@@ -28,13 +23,7 @@ int main() {
 				column.pop_back();
 				// 2^a + 2^b = 2^(a+1)
 				column.push_back(a+1);
-				for (int j = 0; j < column.size(); j++) {
-					cout << column[j] << " ";
-				}
-				cout << endl;
-
 		}
-		cout << "column num (after pop if necessary): " << column.size() << endl;
 	}
 
 	cout << column.size() << endl;
