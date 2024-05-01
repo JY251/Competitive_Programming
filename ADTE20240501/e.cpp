5ec57a68dc979b1f04bf644f88b4549b212ee43f
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-01 18:50:26 +0900

    sub1

--- ADTE20240501/e.cpp
+++ ADTE20240501/e.cpp
@@ -1,17 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
+
+struct Edge {
+	int from, to;	
+};
 
 int main() {
 	int N;
 	cin >> N;
 
-	vector<int> A(N), B(N);
+	vector<vector<Edge>> graph(N+1); // vector of node: each node stores connected edges
 
 	for (int i = 0; i < N; i++) {
-		cin >> A[i] >> B[i];
+		int a, b;
+		cin >> a >> b;
+		graph[a].push_back({a, b});
+		graph[b].push_back({b, a});
 	}
+
+	queue<int> q;
+	vector<int> dist(N+1, -1);
+	dist[1] = 0;
+	q.pop();
+
+	while(!q.empty()) {
+		int cur = q.front();
+		q.pop();
+
+		for (const Edge& e: graph[cur]) {
+			int next = e.to;
+			if (dist[next] == -1) {
+				dist[next] = dist[cur] + 1;
+				q.push(next);
+			}
+		}
+	}
+
+	int res = 0;
+	for (int i = 0; i <= N; i++) {
+		res = max(res, dist[i]);
+	}
+
+	cout << res << endl;
 
 	return 0;
 }
