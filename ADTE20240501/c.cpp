c82c170bf56eb3e22a07e006515987a705767322
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-01 18:23:08 +0900

    AC

--- ADTE20240501/c.cpp
+++ ADTE20240501/c.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -14,14 +15,31 @@ int main() {
 	}
 
 	int count_same_with_pos = 0;
+	// int hit = 0;
+	int blow = 0;
+	unordered_map<int, int> count_a, count_b;
 	for (int i = 0; i < N; i++) {
 		cin >> B[i];
-		if (B[i] == A[i]) {
-			count_same_with_pos++;
-		}
 	}
 
+	for (int i = 0; i < N; i++) {
+		count_a[A[i]]++;
+		count_b[B[i]]++;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (A[i] == B[i]) {
+			count_same_with_pos++;
+		} else if (count_a[A[i]] > 0 && count_b[A[i]] > 0) { // here should not be count_b[B[i]] > 0
+			// the case same char occurs more than once in A does not h appen; So does B
+			blow++;
+		} 
+	}
+
+
+
 	cout << count_same_with_pos << endl;
+	cout << blow << endl;
 
 	return 0;
 }
