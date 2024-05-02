419a815ecf8d42d2374b3a6ec29dfe0e2a2c88a6
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-02 17:14:29 +0900

    Not using carry since unsigned long is enough

--- ADTE202405021630/c.cpp
+++ ADTE202405021630/c.cpp
@@ -15,12 +15,11 @@ int main() {
 	unsigned long power = 1;
 
 	// x_m x_l
-	unsigned long x_m = 0;
 	unsigned long x_l = 0;
 
 	// cout << numeric_limits<long>::max() << endl;
 
-	for (int i = 0; i < 32; i++) {
+	for (int i = 0; i < 64; i++) {
 		// if (i == 31) {
 		// 	if (numeric_limits<long>::max() - x_l < (long)(power * a[i])) {
 		// 		x_m += 1;
@@ -32,17 +31,7 @@ int main() {
 		// cout << power << " (" << i << "; x_l)" << endl;
 	}
 
-	for (int i = 32; i < 64; i++) {
-		x_m += (unsigned long)power * (unsigned long)a[i];
-		power *= 2;
-		// cout << power << " (" << i << "; x_m)" << endl;
-	}
-
-	if (x_m == 0) {
-		cout << x_l << endl;
-	} else {
-		cout << x_m << x_l << endl;
-	}
+	cout << x_l << endl;
 
 	// cout << "debug: " << "x_m: " << x_m << " x_l: " << x_l << " x_m + x_l: " << x_m + x_l << "\n";
 
