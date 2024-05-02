d6f2268e2bab36889cf963ae66c394776e8bd61f
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-02 16:56:59 +0900

    revised mistake in ==; impl carry (not fin)

--- ADTE202405021630/c.cpp
+++ ADTE202405021630/c.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+// #include <climits> // LONG_MAX
+#include <limits>
 
 using namespace std;
 
@@ -16,7 +18,15 @@ int main() {
 	long x_m = 0;
 	long x_l = 0;
 
+	// cout << numeric_limits<long>::max() << endl;
+
 	for (int i = 0; i < 32; i++) {
+		// if (i == 31) {
+		// 	if (numeric_limits<long>::max() - x_l < (long)(power * a[i])) {
+		// 		x_m += 1;
+		// 		x_l = numeric_limits<long>::max() - x_l;
+		// 	}
+		// }
 		x_l += (long)power * (long)a[i];
 		power *= 2;
 	}
@@ -26,7 +36,7 @@ int main() {
 		power *= 2;
 	}
 
-	if (x_m = 0) {
+	if (x_m == 0) {
 		cout << x_l << endl;
 	} else {
 		cout << x_m << x_l << endl;
