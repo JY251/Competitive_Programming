f7469130d829fbaee7212ea49e740ff2d50520ff
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-07 17:54:53 +0900

    sub1 (Ex1: AC)

--- ADTE202405071730/c.cpp
+++ ADTE202405071730/c.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,12 +8,29 @@ int main() {
 	int N;
 	cin >> N;
 
-	vector<int> W(N);
-	vector<int> X(N);
+	vector<pair<int, int>> w_x(N);
 
 	for (int i = 0; i < N; i++) {
-		cin >> W[i] >> X[i];
+		cin >> w_x[i].first >> w_x[i].second;
 	}
 
+	sort(w_x.begin(), w_x.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+		return a.second < b.second;
+	});
+
+	int ans = 0, cnt = 0;
+	for (const auto& p: w_x) {
+		int w = p.first, x = p.second;
+		if (x <= 9) {
+			cnt += w;
+		} else if (x < 18) {
+			ans = max(ans, cnt);
+			cnt = w;
+		} else {
+			ans = max(ans, cnt);
+		}
+	}
+
+	cout << ans << endl;
 	return 0;
 }
