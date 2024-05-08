a832a04bd95314a219fb4cf0d37bd70a4eb902d8
JY251 <101054474+JY251@users.noreply.github.com>
2024-05-08 20:24:32 +0900

    tmp: tried unordered_map but failes

---
+++ ADTE202405082000/c.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<int> c(n);
+	unordered_map<string, int> price;
+	int price_others;
+
+	for (int i = 0; i < n; i++) {
+		cin >> c[i];
+	}
+	cin >> price_others;
+	for (int i = 0; i < m; i++) {
+		
+	}
+
+
+}
