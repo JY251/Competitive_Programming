425d8f894756053fcda869887d0606c6354fb23f
JY251 <101054474+JY251@users.noreply.github.com>
2024-05-12 00:35:28 +0900

    Solved the compiler error by use `static` fun for the comparison fun of `sort`

--- BC130/4.cpp
+++ BC130/4.cpp
@@ -4,21 +4,22 @@ class Solution {
 public:
     int maxPointsInsideSquare(vector<vector<int>>& points, string s) {
 				vector<pair<char, vector<int>>> points_with_tag(s.size());
-				for (int i = 0; i < s.size(); i++) {
+				for (int i = 0; i < (int)s.size(); i++) {
 					points_with_tag[i] = make_pair(s[i], points[i]);
 				}
 				// sort the points by distance from (0, 0) in the accending order
-				sort(points_with_tag.begin(), points_with_tag.end(), compare_points_with_dist);
+				sort(points_with_tag.begin(), points_with_tag.end(), compare_points_with_dist); // sort must use lambda fun
+				// sort(points_with_tag.begin(), points_with_tag.end(), [this](pair<char, vector<int>>& p1, pair<char, vector<int>>& p2){return compare_points_with_dist(p1, p2);});
 
 
 				unordered_map<char, bool> already_appeared;
 				// Initialize already_appeared
-				for (int i = 0; i < s.size(); i++) {
+				for (int i = 0; i < (int)s.size(); i++) {
 					already_appeared[points_with_tag[i].first] = false;
 				}
 
 				// search from nearest to farthest: if the points have the same tag with already-visited points, then that is the maximum square
-				for (int i = 0; i < s.size(); i++) {
+				for (int i = 0; i < (int)s.size(); i++) {
 					if (already_appeared[points_with_tag[i].first]) {
 						int dist = abs(points_with_tag[i].second[0]) > abs(points_with_tag[i].second[0]) ? abs(points_with_tag[i].second[0]) : abs(points_with_tag[i].second[0]);
 						return dist - 1;
@@ -28,8 +29,9 @@ public:
 					}
 				}
     }
-
-		bool compare_points_with_dist(pair<char, vector<int>>& p1, pair<char, vector<int>>& p2) {
+		// without static, the following fun cannot be used in `sort`.
+		// non-static member function does belong to some specific object but static member function does not belong to any object and can be called without any object.
+		static bool compare_points_with_dist(pair<char, vector<int>>& p1, pair<char, vector<int>>& p2) {
 			// dist(x, y) = max(abs(x), abs(y)) here: dist from (0, 0)
 			int dist1 = (abs(p1.second[0]) > abs(p1.second[1])) ? abs(p1.second[0]) : abs(p1.second[1]);
 			int dist2 = (abs(p2.second[0]) > abs(p2.second[1])) ? abs(p2.second[0]) : abs(p2.second[1]);
