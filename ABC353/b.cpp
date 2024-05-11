0f769e51793e27ba0605e7478c3be1521e2daf73
JY251 <101054474+JY251@users.noreply.github.com>
2024-05-11 21:15:27 +0900

    Sub2

---
+++ ABC353/b.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> a(n);
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	int count = 0;
+	int current_in = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (k - current_in < a[i]) {
+			count++;
+			// not to make this `current_in = 0`. (Doing so, you did not add a[i] to the current_in.)
+			current_in = a[i];
+			// cout << "count: " << count << endl;
+		} else {
+			current_in += a[i];
+			// cout << "current_in: " << current_in << endl;
+		}
+	}
+
+	cout << count << endl;
+	return 0;
+}
