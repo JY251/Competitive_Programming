a2de3bc2809f5598169eccdf7714a0156a99c805
JY251 <101054474+JY251@users.noreply.github.com>
2024-05-11 21:43:27 +0900

    Sub1: RE

--- ABC353/d.cpp
+++ ABC353/d.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -7,11 +9,21 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<int> a(n);
+	vector<string> a(n);
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
 
+	int sum = 0;
+	for (int i = 0; i < n-1; i++) {
+		for (int j = i; j < n; j++) {
+			stringstream ss;
+			ss << a[i] << a[j]; // this is not str
+			sum += stoi(ss.str());
+		}
+	}
+
+	cout << sum << endl;
 
 
 	return 0;
