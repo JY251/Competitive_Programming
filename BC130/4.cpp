9dabe1baaa918b9c696d3be662c94d7ff2760b7d
JY251 <101054474+JY251@users.noreply.github.com>
2024-05-12 00:12:12 +0900

    Changed policy: sort the points by distance from (0, 0) and then search from nearest to farthest: if the points have the same tag with already-visited points, then that is the maximum square

--- BC130/4.cpp
+++ BC130/4.cpp
@@ -3,8 +3,22 @@ using namespace std;
 class Solution {
 public:
     int maxPointsInsideSquare(vector<vector<int>>& points, string s) {
+				// unordered_map<char, int> max_dist_per_char;
+				vector<int> dist_per_char(0);
+				// vector<int> appreared_cnt(0); // count the number of appearance of each char
+				for (int i = 0; i < s.size(); i++) {
+					// dist(x, y) = max(abs(x), abs(y)) here: dist from (0, 0)
+					int dist = (abs(points[i][0]) > abs(points[i][1])) ? abs(points[i][0]) : abs(points[i][1]);
+					dist_per_char[i] = dist;
+				}
+
+				// sort the points by distance from (0, 0) in the accending order
+				// search from nearest to farthest: if the points have the same tag with already-visited points, then that is the maximum square
+				vector<int> 
+
+
         vector<int> flattened_dist = flattenArray(points);
-				
+
 				for (int i = findMax(flattened_dist, flattened_dist.size()); i >= 0; i--) {
 
 				}
