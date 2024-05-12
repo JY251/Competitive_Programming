ca8a39899307d68c8cc7261dd2a4265a5a3f33be
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-12 13:32:54 +0900

    O(n) solution: Reverse the list first

--- WC397/4_v2.cpp
+++ WC397/4_v2.cpp
@@ -3,12 +3,26 @@ using namespace std;
 
 class Solution {
 public:
+		vector<int> reverse(vector<int>& arr) {
+			vector<int> reversed_arr;
+			int n = arr.size();
+			for (int i = 0; i < n; i++) {
+				reversed_arr[i] = arr[n-i-1];
+			}
+			return reversed_arr;
+		}
+
     int maximumEnergy(vector<int>& energy, int k) {
+				vector<int> reversed_energy = reverse(energy);
 				int max_total_energy;
 				for (int i = 0; i < k; i++) {
 					int total_energy = 0;
-					for (int j = i; j < (int)energy.size(); j+=k) {
-						total_energy += energy[j];
+					// int naive_total_energy = 0;
+					vector<int> total_energies;
+					for (int j = i; j < (int)reversed_energy.size(); j+=k) {
+						// naive_total_energy += energy[j];
+						total_energy += reversed_energy[j];
+						total_energies.push_back(total_energy);
 					}
 					if (i == 0) {
 						max_total_energy = total_energy;
