a79f0be8f63f817809ef532f28d22dd6753bbfd5
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-12 12:05:34 +0900

    Sub2: initial val is the case you start from i = 0 (if init = 0, then WA for the case all elements are negative)

--- WC397/4.cpp
+++ WC397/4.cpp
@@ -4,8 +4,10 @@ using namespace std;
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-				int max_total_energy = 0;
-				for (int i = 1; i < (int)energy.size(); i++) {
+				// set max as 0 initially will cause problem
+				// if all elements are negative, the max will be 0 though the correct answer should be either the negative element
+				int max_total_energy;
+				for (int i = 0; i < (int)energy.size(); i++) {
 					// each case: start from i
 					int total_energy = energy[i];
 					while(i+k < (int)energy.size()) {
@@ -13,8 +15,12 @@ public:
 						i += k;
 					}
 
-					if (total_energy > max_total_energy) {
+					if (i == 0) {
 						max_total_energy = total_energy;
+					} else {
+						if (total_energy > max_total_energy) {
+							max_total_energy = total_energy;
+						}
 					}
 				}
 				return max_total_energy;
