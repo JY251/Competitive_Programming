5208ad39af8f9e7907485886830e9b2b8645d840
JY251 <101054474+JY251@users.noreply.github.com>
2024-05-12 11:37:16 +0900

    Struct with char, pos, pos

--- WC397/3.cpp
+++ WC397/3.cpp
@@ -3,7 +3,17 @@ using namespace std;
 
 class Solution {
 public:
+		struct char_with_pos {
+			char c;
+			int pos_s;
+			int pos_t;
+		}
     int findPermutationDifference(string s, string t) {
-        
+        // char, int, int
+				vector<char_with_pos> data_char_pos;
+				for (int i = 0; i < s.size(); i++) {
+					data_char_pos
+				}
+				
     }
 };
