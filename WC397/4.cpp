fe0825b609d843950d3bc992753134825b8a87fc
JY251 <101054474+JY251@users.noreply.github.com>
2024-05-12 11:44:59 +0900

    Sub1: WA

--- WC397/4.cpp
+++ WC397/4.cpp
@@ -4,6 +4,19 @@ using namespace std;
 class Solution {
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        
+				int max_total_energy = 0;
+				for (int i = 1; i < energy.size(); i++) {
+					// each case: start from i
+					int total_energy = energy[i];
+					while(i+k < energy.size()) {
+						total_energy += energy[i+k];
+						i += k;
+					}
+
+					if (total_energy > max_total_energy) {
+						max_total_energy = total_energy;
+					}
+				}
+				return max_total_energy;
     }
 };
