4d22708aecd871d640d01c2a31274b6137dd0372
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-18 21:50:47 +0900

    Revised the code: output list of kept cards

--- ABC354/c.cpp
+++ ABC354/c.cpp
@@ -10,15 +10,16 @@ int main() {
 
 	// vector<int> a(n);
 	// vector<int> c(n);
-	vector<pair<int, int>> p(n);
+	vector<vector<int>> p(n);
 
 	for (int i=0; i<n; i++) {
 		// cin >> a[i] >> c[i];
-		cin >> p[i].first >> p[i].second;
+		p[i][0] = i; // original index
+		cin >> p[i][1] >> p[i][2];
 	}
 
-	sort(p.begin(), p.end(), [](pair<int, int> p1, pair<int, int> p2) {
-		return p1.first < p2.first; // sort by "Tsuyosa" (Japanese: 強さ)
+	sort(p.begin(), p.end(), [](vector<int> p1, vector<int> p2) {
+		return p1[1] < p2[1]; // sort by "Tsuyosa" (Japanese: 強さ)
 	});
 
 	for (int i=0; i<n; i++) {
@@ -26,13 +27,22 @@ int main() {
 		for (int j=i+1; j<n; j++) {
 			if (j >= (int)p.size()) {break;}
 			// since i < j，p[i].first < p[j].first
-			if (p[i].second > p[j].second) {
+			if (p[i][2] > p[j][2]) {
 				p.erase(p.begin()+i); // erase takes `pointer to the element` as argument
 			}
 		}
 	}
 
+	sort(p.begin(), p.end(), [](vector<int> p1, vector<int> p2) {
+		return p1[0] < p2[0];
+	});
+
 	cout << p.size() << endl;
+	for (int i = 0; i < (int)p.size(); i++) {
+		cout << p[i][0]+1;
+		if (i!=(int)p.size()-1) cout << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
