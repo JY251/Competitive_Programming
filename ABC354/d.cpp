e2748c5907e60f2882e8ce44efea16852b0931aa
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-18 21:43:34 +0900

    Sub1: WA

---
+++ ABC354/d.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	int area_2 = 0;
+
+	for (int i=a; i<c; i++) {
+		for (int j=b; j<d; j++) {
+			int new_i = i % 4;
+			int new_j = j % 2;
+
+			switch(a) {
+				case 0:
+					switch(b) {
+						case 0:
+							area_2+=1*2;
+							break;
+						case 1:
+							area_2+=(int)(0.5*2);
+							break;
+					}
+					break;
+				case 1:
+					switch(b) {
+						case 0:
+							area_2+=(int)(0.5*2);
+							break;
+						case 1:
+							area_2+=1*2;
+							break;
+					}
+					break;
+				case 2:
+					switch(b) {
+						case 0:
+							break;
+						case 1:
+							area_2+=(int)(0.5*2);
+							break;
+					}
+					break;
+				case 3:
+					switch(b) {
+						case 0:
+							area_2+=(int)(0.5*2);
+							break;
+						case 1:
+							break;
+					}
+					break;
+			}
+		}
+	}
+
+	cout << area_2 << endl;
+
+	return 0;
+}
