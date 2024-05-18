16232f09a57b7fe4a47a1fe1451c016e5ec9cdf6
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-18 21:41:40 +0900

    Sub1: CE

--- ABC354/c.cpp
+++ ABC354/c.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,11 +8,28 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<int> a(n);
-	vector<int> c(n);
-	
+	// vector<int> a(n);
+	// vector<int> c(n);
+	vector<pair<int, int>> p(n);
+
 	for (int i=0; i<n; i++) {
-		cin >> a[i] >> c[i];
+		// cin >> a[i] >> c[i];
+		cin >> p[i].first >> p[i].second;
+	}
+
+	sort(p.begin(), p.end(), [](pair<int, int> p1, pair<int, int> p2) {
+		return p1.first < p2.first; // sort by "Tsuyosa" (Japanese: 強さ)
+	});
+
+	for (int i=0; i<n; i++) {
+		if (i >= p.size()) {break;}
+		for (int j=i+1; j<n; j++) {
+			if (j >= p.size()) {break;}
+			// since i < j，p[i].first < p[j].first
+			if (p[i].second > p[j].second) {
+				p.erase(i);
+			}
+		}
 	}
 
 	return 0;
