53f6397cbf14950590bd85a1bca6fea7b61de3d1
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-19 12:57:17 +0900

    Calc the point cannot over first: WA

--- WC398/4.cpp
+++ WC398/4.cpp
@@ -6,23 +6,42 @@ class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
 				vector<bool> res((int)queries.size());
+				vector<int> indices_cannot_over = isArraySpecial_1(nums);
         for (int i = 0; i < (int)queries.size(); i++) {
 					vector<int> subArray = vector<int>(nums.begin() + queries[i][0], nums.begin() + queries[i][1] + 1);
-					res[i] = isArraySpecial_1(subArray);
+					for (int j = 0; j < (int)indices_cannot_over.size(); j++) {
+						if (queries[i][1] <= indices_cannot_over[j] && indices_cannot_over[j] <= queries[i][1]) {
+							res[i] = false;
+						}
+					}
+					res[i] = true;
 				}
 				return res;
     }
 
-    bool isArraySpecial_1(vector<int>& nums) {
+    vector<int> isArraySpecial_1(vector<int>& nums) {
+				vector<int> res; // res stores the indices of the elements that have the same parity with the next element
 				int first_remainder = nums[0] % 2;
 				int second_remainder;
         for (int i = 0; i < (int)nums.size()-1; i++) {
 					second_remainder = nums[i+1] % 2;
 					if (first_remainder == second_remainder) {
-						return false;
+						res.push_back(i);
 					}
 					first_remainder = second_remainder;
 				}
-				return true;
+				return res;
     }
 };
+
+int main() {
+	Solution sol;
+	vector<int> nums = {3, 4, 1, 2, 6};
+	vector<vector<int>> queries = {{0, 4}};
+	vector<bool> res = sol.isArraySpecial(nums, queries);
+
+	for (int i = 0; i < (int)res.size(); i++) {
+		cout << res[i] << " ";
+	}
+	cout << endl;
+}
