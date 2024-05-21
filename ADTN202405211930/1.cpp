3f6b586e7a26fa5d7af981f540e487e440d8c0bb
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-05-21 20:14:15 +0900

    Impl by alg1: Sample AC 1/4; All 30/38

--- ADTN202405211930/1.cpp
+++ ADTN202405211930/1.cpp
@@ -31,17 +31,77 @@ int main() {
 	// .##
 	// .#.
 
-	// Count the number of # and . in the first row of a and all the rows of b
+	// Count the number of # and . in the each row of a and of b
+	vector<int> num_sharp_row_a(w, 0);
+	vector<int> num_sharp_row_b(w, 0);
+	for (int i=0; i<h; i++) {
+		// int num_sharp_first_row_a = 0;
+		// int num_dot_first_row_a = 0;
+		for (int j=0; j<w; j++) {
+			if (a[i][j] == '#') num_sharp_row_a[j]++;
+		}
+	}
+
+	for (int i=0; i<h; i++) {
+		for (int j=0; j<w; j++) {
+			if (b[i][j] == '#') num_sharp_row_b[j]++;
+		}
+	}
+
+	// Count the number of # and . in the each column of a and of b
+	vector<int> num_sharp_column_a(h, 0);
+	vector<int> num_sharp_column_b(h, 0);
+	for (int j=0; j<w; j++) {
+		for (int i=0; i<h; i++) {
+			if (a[i][j] == '#') num_sharp_column_a[i]++;
+		}
+	}
+	for (int j=0; j<w; j++) {
+		for (int i=0; i<h; i++) {
+			if (b[i][j] == '#') num_sharp_column_b[i]++;
+		}
+	}
 
 	// Shift of the row direction: let the first row of a has `num_sharp_first_row_a`, shift b in row direction so that the first row of b has `num_sharp_first_row_a` #s.
-		// check if other rows of a and b has the same number of #s and .s
+	for (int s=0; s<h; s++) {
+		if (num_sharp_row_b[s] == num_sharp_row_a[0]) {
+			// check if other rows of a and b has the same number of #s and .s
+			int is_num_sharp_row_same = true; // if there are any rows that are not the same, is_num_sharp_row_same will be false
+			for (int i=1; i<h; i++) {
+				// b[0] will be b[s]; b[1] will be b[s+1]; ... ; b[h-1] will be b[(s+h-1)%h]
+				if (num_sharp_row_a[i] != num_sharp_row_b[(i+s)%h]) is_num_sharp_row_same = false;
+			}
+			// if not the same break
+			if (is_num_sharp_row_same == false) break;
 
-		// if not the same break
+			// if the same continue to check the column direction
+			// Shift of the column direction: let the first column of a has `num_sharp_first_column_a`, shift b in column direction so that the first column of b has `num_sharp_first_column_a` #s.
+			for (int t=0; t<w; t++) {
+				if (num_sharp_column_b[t] == num_sharp_column_a[0]) {
+					// check if other columns of a and b has the same number of #s and .s
+					int is_num_sharp_column_same = true;
+					for (int j=1; j<w; j++) {
+						// b[0] will be b[s]; b[1] will be b[s+1]; ... ; b[h-1] will be b[(s+h-1)%h]
+						if (num_sharp_column_a[j] != num_sharp_column_b[(j+t)%w]) is_num_sharp_column_same = false;
+					}
+					// if not the same break
+					if (is_num_sharp_column_same == false) break;
 
-		// if the same continue to check the column direction
-		// Count the number of # and . in the first column of a and all the columns of b
-
-		// Shift of the column direction: let the first column of a has `num_sharp_first_column_a`, shift b in column direction so that the first column of b has `num_sharp_first_column_a` #s.
-			// check ...
-
+					// if the same, check each element of a and b
+					int is_match = true; // if there are any elements that are not the same, is_match will be false
+					for (int i=0; i<h; i++) {
+						for (int j=0; j<w; j++) {
+							if (a[i][j] != b[(i+s)%h][(j+t)%w]) is_match = false;
+						}
+					}
+					if (is_match == true) {
+						cout << "Yes" << endl;
+						return 0;
+					}
+				}
+			}
+		}
+	}
+	cout << "No" << endl;
+	return 0;
 }
