1ecce95560ff799bc4d75e9af206d3cbf2be24eb
JY251 <101054474+JY251@users.noreply.github.com>
2024-06-01 21:48:56 +0900

    Impl two supplementary functions

--- ABC356/c.cpp
+++ ABC356/c.cpp
@@ -6,27 +6,33 @@ vector<vector<int>> all_combinations(int n, int k) {
 	vector<vector<int>> res(0);
 
 	// assert k >= 1
+	assert(k >= 1);
 	// assert n >= k -> Since k>=1, n>=1
+	assert(n >= k);
 
 	// n C k = (n-1) C (k-1) + (n-1) C k (if n >= 1 and k >= 1)
 	// case n = 1: if n=1, since n>=k, k=1 follows, so no recursion is needed
 	// case k = 1:
+	if (k == 1) {
 		for (int i=0; i<n; i++) {
 			vector<int> comb(1, i+1);
 			res.push_back(comb);
 		}
-	//// recursive case
-	// (n-1) C (k-1)
-	vector<vector<int>> combs1 = all_combinations(n-1, k-1);
-	for (auto& comb: combs1) {
-		comb.push_back(n);
-		res.push_back(comb);
 	}
+	//// recursive case
+	else {
+		// (n-1) C (k-1)
+		vector<vector<int>> combs1 = all_combinations(n-1, k-1);
+		for (auto& comb: combs1) {
+			comb.push_back(n);
+			res.push_back(comb);
+		}
 
-	// (n-1) C k
-	vector<vector<int>> combs2 = all_combinations(n-1, k);
-	for (auto& comb: combs2) {
-		res.push_back(comb);
+		// (n-1) C k
+		vector<vector<int>> combs2 = all_combinations(n-1, k);
+		for (auto& comb: combs2) {
+			res.push_back(comb);
+		}
 	}
 	return res;
 }
@@ -34,17 +40,23 @@ vector<vector<int>> all_combinations(int n, int k) {
 bool is_included(vector<int> a, vector<int> b) {
 	bool is_included = true;
 	// for all items in b
-		// bool is_element_included = false;
+	for (int i=0; i<(int)b.size(); i++) {
+		bool is_element_included = false;
 		// for all items in a
-			// if b[i] == a[j]
-				// is_element_included = true
-				// break (the for loop of a)
-			// else
-				// continue
-		// if is_element_included == false
-			// return false
-		// else
-			// continue
+		for (int j=0; j<(int)a.size(); j++) {
+			if (b[i] == a[j]) {
+				is_element_included = true;
+				break; // break (the for loop of a)
+			} else {
+				continue; // continue to check if b is the same as the next element in a
+			}
+		}
+		if (is_element_included == false) {
+			return false; // if one element in b is not included in a, then return false (since b is not included in a)
+		} else {
+			continue; // continue to check if the next element in b is included in a
+		}
+	}
 }
 
 int main() {
