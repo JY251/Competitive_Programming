c65bffd03ccfdc7ee3b0c3ac38df37abcbf56f08
JY251 <101054474+JY251@users.noreply.github.com>
2024-06-02 12:56:50 +0900

    TLE

--- WC400/4.cpp
+++ WC400/4.cpp
@@ -8,9 +8,10 @@ public:
 			vector<int> v(days, 0);
 			for (auto m: meetings) {
 				for (int i=m[0]; i<=m[1]; i++) {
-					v[i] = 1;
+					v[i-1] = 1; // m is 1-indexed but v is 0-indexed
 				}
 			}
+			cout << endl;
 			int ans = 0;
 			for (int i=0; i<days; i++) {
 				ans += (v[i] == 0);
@@ -18,3 +19,10 @@ public:
 			return ans;
     }
 };
+
+int main() {
+	Solution s;
+	vector<vector<int>> mtgs = {{5, 7}, {1, 3}, {9, 10}};
+	cout << s.countDays(10, mtgs) << endl;
+	return 0;
+}
