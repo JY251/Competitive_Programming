0af07d11c9406044f8a54ba7cd07f6ea2b719149
JY251 <101054474+JY251@users.noreply.github.com>
2024-06-04 20:20:49 +0900

    Sub1: WA - AC 0/2; 3/38 (3WA; 32TLE)

--- ADTH202406041930/e.cpp
+++ ADTH202406041930/e.cpp
@@ -2,6 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int power2(int n) {
+	int res = 1;
+	for (int i=0; i<n; i++) {
+		res *= 2;
+	}
+	return res;
+}
+
 int main() {
 	int n;
 	cin >> n;
@@ -17,20 +25,26 @@ int main() {
 	deque<int> list(0);
 
 	// N loop
+	for (int i=0; i<n; i++) {
 		// push_back the ball of 2**a[i] to the list (rightmost)
 		// deque.push_front(2**a[i])
+		list.push_front(power2(a[i]));
 
-		// if list.size() > 1
-			// if (list[0] == list[1])
-				// continue (go to next loop)
-			// else
+		if (list.size() > 1) {
+			if (list[0] == list[1]) {
+				continue; // continue (go to next loop)
+			} else {
 				// new_element
-				// new_element = pop_front()
-				// new_element += pop_front()
-				// push_front(new_element)
-		// else
-			// continue (go to next loop)
-
-		cout << list.size() << endl;
+				int new_element = list.front();
+				list.pop_front();
+				new_element += list.front();
+				list.pop_front();
+				list.push_front(new_element);
+			}
+		} else {
+			continue; // continue (go to next loop)
+		}
+	}
+	cout << list.size() << endl;
 	return 0;
 }
