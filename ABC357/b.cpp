923a00ad25f26cd2efc1f8851cabf0c622615748
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-06-08 21:32:45 +0900

    Sub2: Revise but all WA: no cout

--- ABC357/b.cpp
+++ ABC357/b.cpp
@@ -5,12 +5,26 @@ int main() {
 	string s;
 	cin >> s;
 
-	// NOTE: 'A': 65, 'a': 97 (Upper case is smaller than lower case, so not to confuse that 'A' is larger than 'a')
+	// int lower, upper = 0; -> lower is not initialized as 0 here
+	int lower = 0;
+	int upper = 0;
+	string all_lower, all_upper = s;
 	for (int i=0; i<(int)s.size(); i++) {
-		if ('Z'>= s[i] && s[i] >= 'A') {
-			s[i] = s[i] - 'A' + 'a';
+		if ('a' < s[i] && s[i] <= 'z') {
+			all_upper[i] = all_upper[i] - 'a' + 'A';
+			lower += 1;
+		} else {
+			all_lower[i] = all_lower[i] - 'A' + 'a';
+			upper += 1;
 		}
 	}
+
+	if (lower < upper) {
+		s = all_upper;
+	} else {
+		s = all_lower;
+	}
+
 	cout << s << endl;
 	return 0;
 }
