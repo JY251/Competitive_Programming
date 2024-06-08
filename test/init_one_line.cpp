6d6dcc92e3bcb6f22f7565cc16d0df3564eba835
XYJ080 <101054474+JY251@users.noreply.github.com>
2024-06-08 22:51:23 +0900

    Tests on the grammer regarding initialization of two variables in one line

---
+++ test/init_one_line.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// The following line does not initialize i (Only j is initialized)
+ 	int i,j=0;
+	// ini i, j=0; (The same even you insert the space)
+	cout << "i = " << i << ", j = " << j << endl;
+	return 0;
+}
