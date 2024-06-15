c4787ee983d6db9fe7a65ae1c3a40a07d3883bf8
JY251 <101054474+JY251@users.noreply.github.com>
2024-06-15 22:32:35 +0900

    draft

---
+++ ABC358/b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	int a; // 1e6
+
+	cin >> n >> a;
+
+	vector<int> T(n);
+
+	for (int i=0; i<n; i++) {
+		cin >> T[i];
+	}
+
+	// list to store the time taken to reach the i-th visitor (NOT needed)
+
+	// for (int i=0; i<n; i++) {
+		// first customer (i=0)
+
+		// else
+			// 2nd: first fin <= second com: return second com + A
+			// 2nd: first fin > second com: return first fin + A
+
+		// print i-th
+
+	return 0;
+}
