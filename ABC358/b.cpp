337e70247d7f6d2b03ae966d8f153eb07096735a
JY251 <101054474+JY251@users.noreply.github.com>
2024-06-15 22:37:19 +0900

    Sub1

--- ABC358/b.cpp
+++ ABC358/b.cpp
@@ -13,16 +13,24 @@ int main() {
 		cin >> T[i];
 	}
 
-	// list to store the time taken to reach the i-th visitor (NOT needed)
+	int previous_fin = 0;
 
-	// for (int i=0; i<n; i++) {
+	for (int i=0; i<n; i++) {
 		// first customer (i=0)
-
-		// else
+		if (i == 0) {
+			previous_fin = T[i] + a;
+		} else {
 			// 2nd: first fin <= second com: return second com + A
+			if (previous_fin <= T[i]) {
+				previous_fin = T[i] + a;
+			}
 			// 2nd: first fin > second com: return first fin + A
+			if (previous_fin > T[i])
+				previous_fin = previous_fin + a;
+		}
 
 		// print i-th
-
+		cout << previous_fin << endl;
+	}
 	return 0;
 }
