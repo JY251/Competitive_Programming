949f85ef502b84449b03ba9381fc83b1af51d7ec
JY251 <101054474+JY251@users.noreply.github.com>
2024-06-23 12:25:08 +0900

    AC - Revised the initial value of min as `INT_MAX` (but not 0); Save the original size of `nums` (`nums` is updated); Debug

--- WC403/3.cpp
+++ WC403/3.cpp
@@ -1,4 +1,4 @@
-// Sub1: WA
+// Sub2: AC
 // https://leetcode.com/contest/weekly-contest-403/problems/minimum-average-of-smallest-and-largest-elements/
 #include <bits/stdc++.h>
 using namespace std;
@@ -19,7 +19,7 @@ public:
 		}
 
 		int pop_min(vector<int>& nums) {
-			int min = 0;
+			int min = INT_MAX;
 			int min_index = 0;
 			for (int i=0; i<(int)nums.size(); i++) {
 				if (nums[i] < min) {
@@ -32,7 +32,7 @@ public:
 		}
 
 		double find_min_double(vector<double>& nums) {
-			double min = 0;
+			double min = INT_MAX;;
 			int min_index = 0;
 			for (int i=0; i<(int)nums.size(); i++) {
 				if (nums[i] < min) {
@@ -45,13 +45,24 @@ public:
 
     double minimumAverage(vector<int>& nums) {
 				vector<double> averages;
-				for (int i=0; i<(int)nums.size()/2; i++) {
+				// NOTE: nums will be modified, so the size will change => store the original size
+				int original_size = nums.size();
+				// cout << "Size: " << nums.size() << endl;
+				for (int i=0; i<(int)original_size/2; i++) {
 					int max = pop_max(nums);
 					int min = pop_min(nums);
 					averages.push_back((double)(max + min) / 2.0);
-
+					// cout << "max: " << max << " min: " << min << " avg: " << (double)(max + min) / 2.0 << endl;
 				}
-				int min_avg = find_min_double(averages);
+				double min_avg = find_min_double(averages);
 				return min_avg;
     }
 };
+
+int main() {
+	Solution s;
+	vector<int> nums = {7,8,3,4,15,13,4,1};
+	double result = s.minimumAverage(nums);
+	cout << "Result: " << result << endl;
+	return 0;
+}
